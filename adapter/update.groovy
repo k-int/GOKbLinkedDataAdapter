21bc04c159adbd1acfbbb6f471347cd21d662919
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-11-12 11:15:47 -0500

    updated

--- adapter/update.groovy
+++ adapter/update.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/groovy
 
 // @GrabResolver(name='es', root='https://oss.sonatype.org/content/repositories/releases')
+@GrabResolver(name='mvnRepository', root='http://central.maven.org/maven2/')
 @GrabResolver(name='kint', root='http://projects.k-int.com/nexus-webapp-1.4.0/content/repositories/releases')
 @Grapes([
   @Grab(group='net.sf.opencsv', module='opencsv', version='2.3'),
@@ -9,21 +10,39 @@
   @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0'),
   @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.1.2'),
   @Grab(group='com.k-int', module='goai', version='1.0.2'),
+  @Grab(group='org.slf4j', module='slf4j-api', version='1.7.6'),
+  @Grab(group='org.slf4j', module='jcl-over-slf4j', version='1.7.6'),
+  @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
+  @Grab(group='xerces', module='xercesImpl', version='2.9.1'),
+  @Grab(group='org.apache.jena', module='jena-tdb', version='1.0.2'),
+  @Grab(group='org.apache.jena', module='jena-core', version='2.11.2'),
+  @Grab(group='org.apache.jena', module='jena-arq', version='2.11.2'),
+  @Grab(group='org.apache.jena', module='jena-iri', version='1.0.2'),
+  @Grab(group='org.apache.jena', module='jena-spatial', version='1.0.1'),
+  @Grab(group='org.apache.jena', module='jena-security', version='2.11.2'),
+  @Grab(group='org.apache.jena', module='jena-text', version='1.0.1'),
+  @Grab(group='virtuoso', module='virtjena', version='2'),
+  @Grab(group='virtuoso', module='virtjdbc', version='4.1')
 ])
 
-
-import groovy.util.slurpersupport.GPathResult
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.URLENC
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
 import groovyx.net.http.*
 import org.apache.http.entity.mime.*
 import org.apache.http.entity.mime.content.*
-import java.nio.charset.Charset
 import org.apache.http.*
 import org.apache.http.protocol.*
-import org.apache.log4j.*
-import au.com.bytecode.opencsv.CSVWriter
+import java.nio.charset.Charset
+import static groovy.json.JsonOutput.*
+import virtuoso.jena.driver.*;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.* ;
+import com.hp.hpl.jena.graph.*;
 import java.text.SimpleDateFormat
+import groovy.util.slurpersupport.GPathResult
+import org.apache.log4j.*
 import com.k_int.goai.*;
 
 def config_file = new File('GOKbLinkedDataAdapter-config.groovy')
@@ -38,7 +57,7 @@ OaiClient oaiclient = new OaiClient(host:'https://gokb.k-int.com/gokb/oai/orgs')
 println("Starting...");
 
 oaiclient.getChangesSince(null, 'gokb') { record ->
-  println("Org... ${record}");
+  println("Org... ${record.header.identifier}");
   println("       ${record.metadata.gokb.org.@id}");
   println("       ${record.metadata.gokb.org.name.text()}");
   record.metadata.gokb.org.identifiers.identifier.each {
